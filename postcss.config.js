5485b1030407dbfe732bed62f34369987cc2ad58
josefssonemil <emiljos@student.chalmers.se>
2020-10-03 18:56:08 +0200

    try another fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,24 +1,16 @@
-const purgecss = [
-    '@fullhuman/postcss-purgecss',
-    {
-        // Specify the paths to all of the template files
-        content: [
-            './pages/**/*.{js,jsx,ts,tsx}',
-            './components/**/*.{js,jsx,ts,tsx}',
-        ],
-        // This is the function used to extract class names from the templates
-        defaultExtractor: (content) => {
-            // Capture as liberally as possible, including things like h-(screen-1.5)
-            const broadMatches = content.match(/[^<>"'`\\s]*[^<>"'`\\s:]/g) || [];
-            // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-            const innerMatches = content.match(/[^<>"'`\\s.()]*[^<>"'`\\s.():]/g) || [];
-            return broadMatches.concat(innerMatches);
-        },
-    },
-];
 module.exports = {
-    plugins: [
-        'tailwindcss',
-        ...process.env.NODE_ENV === 'production' ? [purgecss] : []
-    ]
+    plugins: {
+        tailwindcss: {},
+        ...(process.env.NODE_ENV === 'production' ? {
+            '@fullhuman/postcss-purgecss': {
+                // added sections folder and changed extension to jsx
+                content: [
+                    './pages/**/*.{js,jsx,ts,tsx}',
+                    './components/**/*.{js,jsx,ts,tsx}',
+                ],
+                defaultExtractor: content =>
+                    content.match(/[\w-/:]+(?<!:)/g) || [],
+            },
+        } : {}),
+    },
 };
