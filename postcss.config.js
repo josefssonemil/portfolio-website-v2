32dcc7eba2ca503d24dacb78fc18c0eb52d13f6d
josefssonemil <emiljos@student.chalmers.se>
2020-10-03 18:48:20 +0200

    remove autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -19,7 +19,6 @@ const purgecss = [
 module.exports = {
     plugins: [
         'tailwindcss',
-        'autoprefixer ',
         ...process.env.NODE_ENV === 'production' ? [purgecss] : []
     ]
 };
