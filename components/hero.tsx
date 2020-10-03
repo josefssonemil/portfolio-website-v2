ea7a4483694506c2f4cd19f0d12058f0aa26a3d8
josefssonemil <emiljos@student.chalmers.se>
2020-10-03 19:30:38 +0200

    Remove img from hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -23,7 +23,7 @@ const Hero = (props) => {
                 <svg className="s-media" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path d="M0 3v18h24v-18h-24zm22 16l-6.526-6.618-3.445 3.483-3.418-3.525-6.611 6.66 5.051-8-5.051-6 10.029 7.446 9.971-7.446-4.998 6.01 4.998 7.99z" /></svg>
             </div>
 
-            <img className="mx-auto p-6" src="/assets/hero-img.png" />
+
 
 
 
